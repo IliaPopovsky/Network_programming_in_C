fe630bca5a297980db579f641c9bc2d894ab9f5d
Ilia Popovsky <popovskyilia@gmail.com>
2025-01-19 18:37:04 +0300

    first version project

---
+++ echo_TCP_client_IPv4_MOD/echo_TCP_client_IPv4_MOD.c
@@ -0,0 +1,273 @@
+// echo_TCP_client_IPv4_MOD.c
+#include "unp.h"
+#include <stdarg.h>
+#include <syslog.h>		                                                /* for syslog() */
+
+int daemon_proc;		                                                /* set nonzero by daemon_init() */
+
+static void err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int errno_save, n;
+	char buf[MAXLINE];
+
+        //printf("errno = %d\n", errno);
+	errno_save = errno;		                                        /* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, sizeof(buf), fmt, ap);	                                 /* this is safe */
+#else
+	vsprintf(buf, fmt, ap);					          /* this is not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf+n, sizeof(buf)-n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		                                 /* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+void err_quit(const char *fmt, ...)
+{
+	va_list ap;
+        va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void err_sys(const char *fmt, ...)
+{
+	va_list ap;
+        va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+static ssize_t my_read(int fd, char *ptr)
+{
+	static int	read_cnt = 0;
+	static char	*read_ptr;
+	static char	read_buf[MAXLINE];
+
+	if (read_cnt <= 0) {
+again:
+		if ( (read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
+			if (errno == EINTR)
+				goto again;
+			return(-1);
+		} else if (read_cnt == 0)
+			return(0);
+		read_ptr = read_buf;
+	}
+
+	read_cnt--;
+	*ptr = *read_ptr++;
+	return(1);
+}
+
+ssize_t readline(int fd, void *vptr, size_t maxlen)
+{
+	int		n, rc;
+	char	c, *ptr;
+
+	ptr = vptr;
+	for (n = 1; n < maxlen; n++) {
+		if ( (rc = my_read(fd, &c)) == 1) {
+			*ptr++ = c;
+			if (c == '\n')
+				break;	/* newline is stored, like fgets() */
+		} else if (rc == 0) {
+			if (n == 1)
+				return(0);	/* EOF, no data read */
+			else
+				break;		/* EOF, some data was read */
+		} else
+			return(-1);		/* error, errno set by read() */
+	}
+
+	*ptr = 0;	/* null terminate like fgets() */
+	return(n);
+}
+/* end readline */
+
+ssize_t						/* Write "n" bytes to a descriptor. */
+writen(int fd, const void *vptr, size_t n)
+{
+	size_t		nleft;
+	ssize_t		nwritten;
+	const char	*ptr;
+
+	ptr = vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+			if (errno == EINTR)
+				nwritten = 0;		/* and call write() again */
+			else
+				return(-1);			/* error */
+		}
+
+		nleft -= nwritten;
+		ptr   += nwritten;
+	}
+	return(n);
+}
+/* end writen */
+
+void
+Writen(int fd, void *ptr, size_t nbytes)
+{
+	if (writen(fd, ptr, nbytes) != nbytes)
+		err_sys("writen error");
+}
+
+
+ssize_t Readline(int fd, void *ptr, size_t maxlen)
+{
+	ssize_t		n;
+
+	if ( (n = readline(fd, ptr, maxlen)) < 0)
+		err_sys("readline error");
+	return(n);
+}
+
+void str_cli(FILE *fp, int sockfd)
+{
+	char	sendline[MAXLINE], recvline[MAXLINE];
+
+	while (fgets(sendline, MAXLINE, fp) != NULL) {
+
+		Writen(sockfd, sendline, strlen(sendline));
+
+		if (Readline(sockfd, recvline, MAXLINE) == 0)
+			err_quit("str_cli: server terminated prematurely");
+
+		fputs(recvline, stdout);
+	}
+}
+
+char *sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+    char		portstr[7];
+    static char str[128];		/* Unix domain is largest */
+
+	switch (sa->sa_family) {
+	case AF_INET: {
+		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+		if (inet_ntop(AF_INET, &sin->sin_addr, str, sizeof(str)) == NULL)
+			return(NULL);
+		if (ntohs(sin->sin_port) != 0) {
+			snprintf(portstr, sizeof(portstr), ".%d", ntohs(sin->sin_port));
+			strcat(str, portstr);
+		}
+		return(str);
+	}
+/* end sock_ntop */
+
+#ifdef	IPV6
+	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6 *) sa;
+
+		if (inet_ntop(AF_INET6, &sin6->sin6_addr, str, sizeof(str)) == NULL)
+			return(NULL);
+		if (ntohs(sin6->sin6_port) != 0) {
+			snprintf(portstr, sizeof(portstr), ".%d", ntohs(sin6->sin6_port));
+			strcat(str, portstr);
+		}
+		return(str);
+	}
+#endif
+
+#ifdef	AF_UNIX
+	case AF_UNIX: {
+		struct sockaddr_un	*unp = (struct sockaddr_un *) sa;
+
+			/* OK to have no pathname bound to the socket: happens on
+			   every connect() unless client calls bind() first. */
+		if (unp->sun_path[0] == 0)
+			strcpy(str, "(no pathname bound)");
+		else
+			snprintf(str, sizeof(str), "%s", unp->sun_path);
+		return(str);
+	}
+#endif
+
+#ifdef	HAVE_SOCKADDR_DL_STRUCT
+	case AF_LINK: {
+		struct sockaddr_dl	*sdl = (struct sockaddr_dl *) sa;
+
+		if (sdl->sdl_nlen > 0)
+			snprintf(str, sizeof(str), "%*s",
+					 sdl->sdl_nlen, &sdl->sdl_data[0]);
+		else
+			snprintf(str, sizeof(str), "AF_LINK, index=%d", sdl->sdl_index);
+		return(str);
+	}
+#endif
+	default:
+		snprintf(str, sizeof(str), "sock_ntop: unknown AF_xxx: %d, len %d",
+				 sa->sa_family, salen);
+		return(str);
+	}
+    return (NULL);
+}
+
+
+int main(int argc, char **argv)
+{
+   int i, sockfd[5];
+   struct sockaddr_in servaddr, cliaddr;                                     // Структура адреса сервера и клиента.
+   int counter_reads = 0;
+   socklen_t len;
+   
+   if(argc != 3)
+       err_quit("usage: a.out <IPaddress> and number port");
+       
+   for(i = 0; i < 5; i++){
+      if((sockfd[i] = socket(AF_INET, SOCK_STREAM, 0)) < 0)            // Создание сокета, функция socket() создает объект сокета в ядре ОС с помощью системного вызова socket, который имеет номер
+         err_sys("socket error");                                      // 41 для 64-битной ОС Linux и номер 359 для 32-битной ОС LINUX, а в других ОС какой-то другой номер.
+                                                                       // Системный вызов socket() создаёт конечную точку соединения и возвращает файловый дескриптор, указывающий на эту точку. 
+                                                                       // Возвращаемый при успешном выполнении файловый дескриптор будет иметь самый маленький номер, который не используется 
+                                                                       // процессом. 
+                                                                                                                                                   
+      bzero(&servaddr, sizeof(servaddr));                              // Инициализируем всю структуру адреса сервера нулями. Функция bzero() имеет свою реализацию в некоторых ОС. У нас она
+                                                                       // имеет вид функционального макроса, который вызывает библиотечную функцию memset() и располагается в файле unp.h
+                                                                       // на случай, если в какой-то ОС не будет своей реализации функции bzero().
+      bzero(&cliaddr, sizeof(cliaddr));  
+   
+      servaddr.sin_family = AF_INET;                                   // Присваиваем значение полю структуры адреса сервера, отвечающему за семейство адресов.     
+      servaddr.sin_port = htons(atoi(argv[2]));                        // Присваиваем значение полю структуры адреса сервера, отвечающему за номер порта. Библиотечная функция htons() преобразует
+                                                                       // двузначный номер порта, принятый в качестве аргумента, в требуемый формат, путем вращения влево битов значения с помощью
+                                                                       // инструкции ЦП rol.
+      if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)         // Присваиваем значение полю структуры адреса сервера, отвечающему за IP-адрес. Библиотечная функция inet_pton() преобразует
+         err_quit("inet_pton error for %s", argv[1]);                  // аргумент командной строки в символах ASCII(например 206.168.112.96) в двоичный формат.
+      
+      printf("Наш клиент имеет PID = %d\n", getpid());
+      printf("Наш клиент обращается к серверу с номером порта = %d\n", htons(servaddr.sin_port));
+      if(connect(sockfd[i], (SA *)&servaddr, sizeof(servaddr)) < 0)    // Функция connect() устанавливает соединение нашего сокета sockfd с сервером, адрес сокета которого содержится в структуре
+         err_sys("connect error");                                     // адреса сервера servaddr. Функция connect() использует системный вызов connect, который имеет номер 42 для 64-битной 
+                                                                       // ОС Linux и номер 362 для 32-битной ОС LINUX, а в других ОС какой-то другой номер. 
+                                                                       // Системный вызов connect() устанавливает соединение с сокетом, заданным файловый дескриптором sockfd, ссылающимся
+                                                                       // на адрес servaddr. Аргумент sizeof(servaddr) определяет размер servaddr. Формат адреса в servaddr определяется адресным 
+                                                                       // пространством сокета sockfd. 
+      cliaddr.sin_family = AF_INET;                                                                  
+      len = sizeof(cliaddr);  // = MAXSOCKADDR;                                                                   
+      if(getsockname(sockfd[i], (SA *)&cliaddr, &len) < 0)
+         printf("Не удалось получить локальный адрес протокола.\n");
+      printf("Наш клиент имеет локальный IP-адрес (по getsockname()) = %s\n", sock_ntop((SA *)&cliaddr, len)); 
+      printf("Наш клиент имеет номер порта (по getsockname()) = %d\n", ntohs(cliaddr.sin_port));                      
+   }
+   str_cli(stdin, sockfd[0]);                         /* эта функция выполняет все необходимые действия со стороны клиента */  
+   
+   exit(0);
+} 
+     
+                                                  
