e1a8d2fb511b2bc1edd4698fe285e96c89ef5390
Ilia Popovsky <popovskyilia@gmail.com>
2025-03-30 15:51:18 +0300

    first version project

---
+++ show_local_addr_client_UDP_IPv4/show_local_addr_client_UDP_IPv4.c
@@ -0,0 +1,154 @@
+// show_local_addr_client_UDP_IPv4.c
+#include	"unp.h"
+
+#include <stdarg.h>
+#include <syslog.h>		                                                /* for syslog() */
+
+int daemon_proc;		                                                /* set nonzero by daemon_init() */
+
+static void err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int errno_save, n;
+	char buf[MAXLINE];
+
+	errno_save = errno;		                                        /* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, sizeof(buf), fmt, ap);	                                 /* this is safe */
+#else
+	vsprintf(buf, fmt, ap);					          /* this is not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf+n, sizeof(buf)-n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		                                 /* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+void err_quit(const char *fmt, ...)
+{
+	va_list ap;
+        va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void err_sys(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+char *sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+    char		portstr[7];
+    static char str[128];		/* Unix domain is largest */
+
+	switch (sa->sa_family) {
+	case AF_INET: {
+		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+		if (inet_ntop(AF_INET, &sin->sin_addr, str, sizeof(str)) == NULL)
+			return(NULL);
+		if (ntohs(sin->sin_port) != 0) {
+			snprintf(portstr, sizeof(portstr), ".%d", ntohs(sin->sin_port));
+			strcat(str, portstr);
+		}
+		return(str);
+	}
+/* end sock_ntop */
+
+#ifdef	IPV6
+	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6 *) sa;
+
+		if (inet_ntop(AF_INET6, &sin6->sin6_addr, str, sizeof(str)) == NULL)
+			return(NULL);
+		if (ntohs(sin6->sin6_port) != 0) {
+			snprintf(portstr, sizeof(portstr), ".%d", ntohs(sin6->sin6_port));
+			strcat(str, portstr);
+		}
+		return(str);
+	}
+#endif
+
+#ifdef	AF_UNIX
+	case AF_UNIX: {
+		struct sockaddr_un	*unp = (struct sockaddr_un *) sa;
+
+			/* OK to have no pathname bound to the socket: happens on
+			   every connect() unless client calls bind() first. */
+		if (unp->sun_path[0] == 0)
+			strcpy(str, "(no pathname bound)");
+		else
+			snprintf(str, sizeof(str), "%s", unp->sun_path);
+		return(str);
+	}
+#endif
+
+#ifdef	HAVE_SOCKADDR_DL_STRUCT
+	case AF_LINK: {
+		struct sockaddr_dl	*sdl = (struct sockaddr_dl *) sa;
+
+		if (sdl->sdl_nlen > 0)
+			snprintf(str, sizeof(str), "%*s",
+					 sdl->sdl_nlen, &sdl->sdl_data[0]);
+		else
+			snprintf(str, sizeof(str), "AF_LINK, index=%d", sdl->sdl_index);
+		return(str);
+	}
+#endif
+	default:
+		snprintf(str, sizeof(str), "sock_ntop: unknown AF_xxx: %d, len %d",
+				 sa->sa_family, salen);
+		return(str);
+	}
+    return (NULL);
+}
+
+char *Sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+	char	*ptr;
+
+	if ( (ptr = sock_ntop(sa, salen)) == NULL)
+		err_sys("sock_ntop error");	/* inet_ntop() sets errno */
+	return(ptr);
+}
+
+int main(int argc, char **argv)
+{
+	int sockfd;
+	socklen_t len;
+	struct sockaddr_in cliaddr, servaddr;
+
+	if (argc != 2)
+		err_quit("usage: udpcli <IPaddress>");
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+	connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+
+	len = sizeof(cliaddr);
+	getsockname(sockfd, (SA *) &cliaddr, &len);
+	printf("local address %s\n", Sock_ntop((SA *) &cliaddr, len));
+
+	exit(0);
+}
+
