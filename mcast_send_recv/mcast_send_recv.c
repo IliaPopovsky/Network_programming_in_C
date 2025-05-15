073995223bc48667a240ea0d04472e9f9efd2ca9
Ilia Popovsky <popovskyilia@gmail.com>
2025-05-15 13:05:53 +0300

    first version project

---
+++ mcast_send_recv/mcast_send_recv.c
@@ -0,0 +1,462 @@
+// mcast_send_recv.c
+#include "unp.h"
+
+#include <stdarg.h>
+#include <syslog.h>		                                                /* for syslog() */
+
+#include <sys/utsname.h>
+
+#define SENDRATE 5		/* send one datagram every five seconds */
+
+int daemon_proc;		                                                /* set nonzero by daemon_init() */
+
+static void err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int errno_save, n;
+	char buf[MAXLINE];
+
+	errno_save = errno;		                                        /* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, sizeof(buf), fmt, ap);	                                 /* this is safe */
+#else
+	vsprintf(buf, fmt, ap);					          /* this is not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf+n, sizeof(buf)-n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		                                 /* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+void err_quit(const char *fmt, ...)
+{
+	va_list ap;
+        va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void err_sys(const char *fmt, ...)
+{
+	va_list ap;
+        va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void
+err_msg(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, LOG_INFO, fmt, ap);
+	va_end(ap);
+	return;
+}
+
+int Socket(int family, int type, int protocol)                                 // Наша собственная функция-обертка для функции socket.
+{
+  int n;
+  if( (n = socket(family, type, protocol)) < 0)
+       err_sys("socket error");
+  return (n);
+}
+
+void Bind(int fd, const struct sockaddr *sa, socklen_t salen)                  // Наша собственная функция-обертка для функции bind.
+{
+	if (bind(fd, sa, salen) < 0)
+		err_sys("bind error");
+}
+
+
+
+void Write(int fd, void *ptr, size_t nbytes)                                   // Наша собственная функция-обертка для функции write.
+{
+	if (write(fd, ptr, nbytes) != nbytes)
+		err_sys("write error");
+}
+
+void Close(int fd)                                                             // Наша собственная функция-обертка для функции close.
+{
+	if (close(fd) == -1)
+		err_sys("close error");
+}
+
+char *
+sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+    char		portstr[8];
+    static char str[128];		/* Unix domain is largest */
+
+	switch (sa->sa_family) {
+	case AF_INET: {
+		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+		if (inet_ntop(AF_INET, &sin->sin_addr, str, sizeof(str)) == NULL)
+			return(NULL);
+		if (ntohs(sin->sin_port) != 0) {
+			snprintf(portstr, sizeof(portstr), ":%d", ntohs(sin->sin_port));
+			strcat(str, portstr);
+		}
+		return(str);
+	}
+/* end sock_ntop */
+
+#ifdef	IPV6
+	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6 *) sa;
+
+		str[0] = '[';
+		if (inet_ntop(AF_INET6, &sin6->sin6_addr, str + 1, sizeof(str) - 1) == NULL)
+			return(NULL);
+		if (ntohs(sin6->sin6_port) != 0) {
+			snprintf(portstr, sizeof(portstr), "]:%d", ntohs(sin6->sin6_port));
+			strcat(str, portstr);
+			return(str);
+		}
+		return (str + 1);
+	}
+#endif
+
+#ifdef	AF_UNIX
+	case AF_UNIX: {
+		struct sockaddr_un	*unp = (struct sockaddr_un *) sa;
+
+			/* OK to have no pathname bound to the socket: happens on
+			   every connect() unless client calls bind() first. */
+		if (unp->sun_path[0] == 0)
+			strcpy(str, "(no pathname bound)");
+		else
+			snprintf(str, sizeof(str), "%s", unp->sun_path);
+		return(str);
+	}
+#endif
+
+#ifdef	HAVE_SOCKADDR_DL_STRUCT
+	case AF_LINK: {
+		struct sockaddr_dl	*sdl = (struct sockaddr_dl *) sa;
+
+		if (sdl->sdl_nlen > 0)
+			snprintf(str, sizeof(str), "%*s (index %d)",
+					 sdl->sdl_nlen, &sdl->sdl_data[0], sdl->sdl_index);
+		else
+			snprintf(str, sizeof(str), "AF_LINK, index=%d", sdl->sdl_index);
+		return(str);
+	}
+#endif
+	default:
+		snprintf(str, sizeof(str), "sock_ntop: unknown AF_xxx: %d, len %d",
+				 sa->sa_family, salen);
+		return(str);
+	}
+    return (NULL);
+}
+
+char *
+Sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+	char	*ptr;
+
+	if ( (ptr = sock_ntop(sa, salen)) == NULL)
+		err_sys("sock_ntop error");	/* inet_ntop() sets errno */
+	return(ptr);
+}
+
+
+int
+family_to_level(int family)
+{
+	switch (family) {
+	case AF_INET:
+		return IPPROTO_IP;
+#ifdef	IPV6
+	case AF_INET6:
+		return IPPROTO_IPV6;
+#endif
+	default:
+		return -1;
+	}
+}
+
+int
+Family_to_level(int family)
+{
+	int		rc;
+
+	if ( (rc = family_to_level(family)) < 0)
+		err_sys("family_to_level error");
+
+	return(rc);
+}
+
+
+int
+udp_client(const char *host, const char *serv, void **saptr, socklen_t *lenp)
+{
+	int				sockfd, n;
+	struct addrinfo	hints, *res, *ressave;
+
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+
+	if ( (n = getaddrinfo(host, serv, &hints, &res)) != 0)
+		err_quit("udp_client error for %s, %s: %s",
+				 host, serv, gai_strerror(n));
+	ressave = res;
+
+	do {
+		sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (sockfd >= 0)
+			break;		/* success */
+	} while ( (res = res->ai_next) != NULL);
+
+	if (res == NULL)	/* errno set from final socket() */
+		err_sys("udp_client error for %s, %s", host, serv);
+
+	*saptr = malloc(res->ai_addrlen);
+	memcpy(*saptr, res->ai_addr, res->ai_addrlen);
+	*lenp = res->ai_addrlen;
+
+	freeaddrinfo(ressave);
+
+	return(sockfd);
+}
+/* end udp_client */
+
+int
+mcast_join(int sockfd, const SA *grp, socklen_t grplen,
+		   const char *ifname, u_int ifindex)
+{
+#ifdef MCAST_JOIN_GROUP
+	struct group_req req;
+	if (ifindex > 0) {
+		req.gr_interface = ifindex;
+	} else if (ifname != NULL) {
+		if ( (req.gr_interface = if_nametoindex(ifname)) == 0) {
+			errno = ENXIO;	/* i/f name not found */
+			return(-1);
+		}
+	} else
+		req.gr_interface = 0;
+	if (grplen > sizeof(req.gr_group)) {
+		errno = EINVAL;
+		return -1;
+	}
+	memcpy(&req.gr_group, grp, grplen);
+	return (setsockopt(sockfd, family_to_level(grp->sa_family),
+			MCAST_JOIN_GROUP, &req, sizeof(req)));
+#else
+/* end mcast_join1 */
+
+/* include mcast_join2 */
+	switch (grp->sa_family) {
+	case AF_INET: {
+		struct ip_mreq		mreq;
+		struct ifreq		ifreq;
+
+		memcpy(&mreq.imr_multiaddr,
+			   &((const struct sockaddr_in *) grp)->sin_addr,
+			   sizeof(struct in_addr));
+
+		if (ifindex > 0) {
+			if (if_indextoname(ifindex, ifreq.ifr_name) == NULL) {
+				errno = ENXIO;	/* i/f index not found */
+				return(-1);
+			}
+			goto doioctl;
+		} else if (ifname != NULL) {
+			strncpy(ifreq.ifr_name, ifname, IFNAMSIZ);
+doioctl:
+			if (ioctl(sockfd, SIOCGIFADDR, &ifreq) < 0)
+				return(-1);
+			memcpy(&mreq.imr_interface,
+				   &((struct sockaddr_in *) &ifreq.ifr_addr)->sin_addr,
+				   sizeof(struct in_addr));
+		} else
+			mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+
+		return(setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP,
+						  &mreq, sizeof(mreq)));
+	}
+/* end mcast_join2 */
+
+/* include mcast_join3 */
+#ifdef	IPV6
+#ifndef	IPV6_JOIN_GROUP		/* APIv0 compatibility */
+#define	IPV6_JOIN_GROUP		IPV6_ADD_MEMBERSHIP
+#endif
+	case AF_INET6: {
+		struct ipv6_mreq	mreq6;
+
+		memcpy(&mreq6.ipv6mr_multiaddr,
+			   &((const struct sockaddr_in6 *) grp)->sin6_addr,
+			   sizeof(struct in6_addr));
+
+		if (ifindex > 0) {
+			mreq6.ipv6mr_interface = ifindex;
+		} else if (ifname != NULL) {
+			if ( (mreq6.ipv6mr_interface = if_nametoindex(ifname)) == 0) {
+				errno = ENXIO;	/* i/f name not found */
+				return(-1);
+			}
+		} else
+			mreq6.ipv6mr_interface = 0;
+
+		return(setsockopt(sockfd, IPPROTO_IPV6, IPV6_JOIN_GROUP,
+						  &mreq6, sizeof(mreq6)));
+	}
+#endif
+
+	default:
+		errno = EAFNOSUPPORT;
+		return(-1);
+	}
+#endif
+}
+/* end mcast_join3 */
+
+void
+Mcast_join(int sockfd, const SA *grp, socklen_t grplen,
+		   const char *ifname, u_int ifindex)
+{
+	if (mcast_join(sockfd, grp, grplen, ifname, ifindex) < 0)
+		err_sys("mcast_join error");
+}
+
+void
+send_all(int sendfd, SA *sadest, socklen_t salen)
+{
+	char		line[MAXLINE];		/* hostname and process ID */
+	struct utsname	myname;
+
+	if (uname(&myname) < 0)
+		err_sys("uname error");;
+	snprintf(line, sizeof(line), "%s, %d\n", myname.nodename, getpid());
+
+	for ( ; ; ) {
+		sendto(sendfd, line, strlen(line), 0, sadest, salen);
+
+		sleep(SENDRATE);
+	}
+}
+
+void
+recv_all(int recvfd, socklen_t salen)
+{
+	int					n;
+	char				line[MAXLINE+1];
+	socklen_t			len;
+	struct sockaddr		*safrom;
+
+	safrom = malloc(salen);
+
+	for ( ; ; ) {
+		len = salen;
+		n = recvfrom(recvfd, line, MAXLINE, 0, safrom, &len);
+
+		line[n] = 0;	/* null terminate */
+		printf("from %s: %s", Sock_ntop(safrom, len), line);
+	}
+}
+
+int
+sockfd_to_family(int sockfd)
+{
+	struct sockaddr_storage ss;
+	socklen_t	len;
+
+	len = sizeof(ss);
+	if (getsockname(sockfd, (SA *) &ss, &len) < 0)
+		return(-1);
+	return(ss.ss_family);
+}
+/* end sockfd_to_family */
+
+int
+Sockfd_to_family(int sockfd)
+{
+	int		rc;
+
+	if ( (rc = sockfd_to_family(sockfd)) < 0)
+		err_sys("sockfd_to_family error");
+
+	return(rc);
+}
+
+int
+mcast_set_loop(int sockfd, int onoff)
+{
+	switch (sockfd_to_family(sockfd)) {
+	case AF_INET: {
+		u_char		flag;
+
+		flag = onoff;
+		return(setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_LOOP,
+						  &flag, sizeof(flag)));
+	}
+
+#ifdef	IPV6
+	case AF_INET6: {
+		u_int		flag;
+
+		flag = onoff;
+		return(setsockopt(sockfd, IPPROTO_IPV6, IPV6_MULTICAST_LOOP,
+						  &flag, sizeof(flag)));
+	}
+#endif
+
+	default:
+		errno = EAFNOSUPPORT;
+		return(-1);
+	}
+}
+/* end mcast_set_loop */
+
+void
+Mcast_set_loop(int sockfd, int onoff)
+{
+	if (mcast_set_loop(sockfd, onoff) < 0)
+		err_sys("mcast_set_loop error");
+}
+
+int
+main(int argc, char **argv)
+{
+	int					sendfd, recvfd;
+	const int			on = 1;
+	socklen_t			salen;
+	struct sockaddr		*sasend, *sarecv;
+
+	if (argc != 3)
+		err_quit("usage: sendrecv <IP-multicast-address> <port#>");
+
+	sendfd = udp_client(argv[1], argv[2], (void **) &sasend, &salen);
+
+	recvfd = Socket(sasend->sa_family, SOCK_DGRAM, 0);
+
+	setsockopt(recvfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+	sarecv = malloc(salen);
+	memcpy(sarecv, sasend, salen);
+	Bind(recvfd, sarecv, salen);
+
+	Mcast_join(recvfd, sasend, salen, NULL, 0);
+	Mcast_set_loop(sendfd, 0);
+
+	if (fork() == 0)
+		recv_all(recvfd, salen);		/* child -> receives */
+
+	send_all(sendfd, sasend, salen);	/* parent -> sends */
+}
