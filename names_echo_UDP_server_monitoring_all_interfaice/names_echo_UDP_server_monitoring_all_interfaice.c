27cc3153479462658d3edf56057bd869192f3029
Ilia Popovsky <popovskyilia@gmail.com>
2025-05-25 15:25:42 +0300

    first version project

---
+++ names_echo_UDP_server_monitoring_all_interfaice/names_echo_UDP_server_monitoring_all_interfaice.c
@@ -0,0 +1,580 @@
+// names_echo_UDP_server_monitoring_all_interfaice.c
+#include	"unpifi.h"
+
+#include	<stdarg.h>		/* ANSI C header file */
+#include	<syslog.h>		/* for syslog() */
+
+int		daemon_proc;		/* set nonzero by daemon_init() */
+
+static void	err_doit(int, int, const char *, va_list);
+
+/* Nonfatal error related to system call
+ * Print message and return */
+
+static void
+err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int		errno_save, n;
+	char	buf[MAXLINE + 1];
+
+	errno_save = errno;		/* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, MAXLINE, fmt, ap);	/* safe */
+#else
+	vsprintf(buf, fmt, ap);					/* not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf + n, MAXLINE - n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		/* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+void
+err_ret(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_INFO, fmt, ap);
+	va_end(ap);
+	return;
+}
+
+/* Fatal error related to system call
+ * Print message and terminate */
+
+void
+err_sys(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void
+err_quit(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+char *
+sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+    char		portstr[8];
+    static char str[128];		/* Unix domain is largest */
+
+	switch (sa->sa_family) {
+	case AF_INET: {
+		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+		if (inet_ntop(AF_INET, &sin->sin_addr, str, sizeof(str)) == NULL)
+			return(NULL);
+		if (ntohs(sin->sin_port) != 0) {
+			snprintf(portstr, sizeof(portstr), ":%d", ntohs(sin->sin_port));
+			strcat(str, portstr);
+		}
+		return(str);
+	}
+/* end sock_ntop */
+
+#ifdef	IPV6
+	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6 *) sa;
+
+		str[0] = '[';
+		if (inet_ntop(AF_INET6, &sin6->sin6_addr, str + 1, sizeof(str) - 1) == NULL)
+			return(NULL);
+		if (ntohs(sin6->sin6_port) != 0) {
+			snprintf(portstr, sizeof(portstr), "]:%d", ntohs(sin6->sin6_port));
+			strcat(str, portstr);
+			return(str);
+		}
+		return (str + 1);
+	}
+#endif
+
+#ifdef	AF_UNIX
+	case AF_UNIX: {
+		struct sockaddr_un	*unp = (struct sockaddr_un *) sa;
+
+			/* OK to have no pathname bound to the socket: happens on
+			   every connect() unless client calls bind() first. */
+		if (unp->sun_path[0] == 0)
+			strcpy(str, "(no pathname bound)");
+		else
+			snprintf(str, sizeof(str), "%s", unp->sun_path);
+		return(str);
+	}
+#endif
+
+#ifdef	HAVE_SOCKADDR_DL_STRUCT
+	case AF_LINK: {
+		struct sockaddr_dl	*sdl = (struct sockaddr_dl *) sa;
+
+		if (sdl->sdl_nlen > 0)
+			snprintf(str, sizeof(str), "%*s (index %d)",
+					 sdl->sdl_nlen, &sdl->sdl_data[0], sdl->sdl_index);
+		else
+			snprintf(str, sizeof(str), "AF_LINK, index=%d", sdl->sdl_index);
+		return(str);
+	}
+#endif
+	default:
+		snprintf(str, sizeof(str), "sock_ntop: unknown AF_xxx: %d, len %d",
+				 sa->sa_family, salen);
+		return(str);
+	}
+    return (NULL);
+}
+
+char *
+Sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+	char	*ptr;
+
+	if ( (ptr = sock_ntop(sa, salen)) == NULL)
+		err_sys("sock_ntop error");	/* inet_ntop() sets errno */
+	return(ptr);
+}
+
+struct ifi_info *
+get_ifi_info(int family, int doaliases)
+{
+	struct ifi_info		*ifi, *ifihead, **ifipnext;
+	int					sockfd, len, lastlen, flags, myflags, idx = 0, hlen = 0;
+	char				*ptr, *buf, lastname[IFNAMSIZ], *cptr, *haddr, *sdlname;
+	struct ifconf		ifc;
+	struct ifreq		*ifr, ifrcopy;
+	struct sockaddr_in	*sinptr;
+	struct sockaddr_in6	*sin6ptr;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	lastlen = 0;
+	len = 100 * sizeof(struct ifreq);	/* initial buffer size guess */
+	for ( ; ; ) {
+		buf = malloc(len);
+		ifc.ifc_len = len;
+		ifc.ifc_buf = buf;
+		if (ioctl(sockfd, SIOCGIFCONF, &ifc) < 0) {
+			if (errno != EINVAL || lastlen != 0)
+				err_sys("ioctl error");
+		} else {
+			if (ifc.ifc_len == lastlen)
+				break;		/* success, len has not changed */
+			lastlen = ifc.ifc_len;
+		}
+		len += 10 * sizeof(struct ifreq);	/* increment */
+		free(buf);
+	}
+	ifihead = NULL;
+	ifipnext = &ifihead;
+	lastname[0] = 0;
+	sdlname = NULL;
+/* end get_ifi_info1 */
+
+/* include get_ifi_info2 */
+	for (ptr = buf; ptr < buf + ifc.ifc_len; ) {
+		ifr = (struct ifreq *) ptr;
+
+#ifdef	HAVE_SOCKADDR_SA_LEN
+		len = max(sizeof(struct sockaddr), ifr->ifr_addr.sa_len);
+#else
+		switch (ifr->ifr_addr.sa_family) {
+#ifdef	IPV6
+		case AF_INET6:	
+			len = sizeof(struct sockaddr_in6);
+			break;
+#endif
+		case AF_INET:	
+		default:	
+			len = sizeof(struct sockaddr);
+			break;
+		}
+#endif	/* HAVE_SOCKADDR_SA_LEN */
+		ptr += sizeof(ifr->ifr_name) + len;	/* for next one in buffer */
+
+#ifdef	HAVE_SOCKADDR_DL_STRUCT
+		/* assumes that AF_LINK precedes AF_INET or AF_INET6 */
+		if (ifr->ifr_addr.sa_family == AF_LINK) {
+			struct sockaddr_dl *sdl = (struct sockaddr_dl *)&ifr->ifr_addr;
+			sdlname = ifr->ifr_name;
+			idx = sdl->sdl_index;
+			haddr = sdl->sdl_data + sdl->sdl_nlen;
+			hlen = sdl->sdl_alen;
+		}
+#endif
+
+		if (ifr->ifr_addr.sa_family != family)
+			continue;	/* ignore if not desired address family */
+
+		myflags = 0;
+		if ( (cptr = strchr(ifr->ifr_name, ':')) != NULL)
+			*cptr = 0;		/* replace colon with null */
+		if (strncmp(lastname, ifr->ifr_name, IFNAMSIZ) == 0) {
+			if (doaliases == 0)
+				continue;	/* already processed this interface */
+			myflags = IFI_ALIAS;
+		}
+		memcpy(lastname, ifr->ifr_name, IFNAMSIZ);
+
+		ifrcopy = *ifr;
+		ioctl(sockfd, SIOCGIFFLAGS, &ifrcopy);
+		flags = ifrcopy.ifr_flags;
+		if ((flags & IFF_UP) == 0)
+			continue;	/* ignore if interface not up */
+/* end get_ifi_info2 */
+
+/* include get_ifi_info3 */
+		ifi = calloc(1, sizeof(struct ifi_info));
+		*ifipnext = ifi;			/* prev points to this new one */
+		ifipnext = &ifi->ifi_next;	/* pointer to next one goes here */
+
+		ifi->ifi_flags = flags;		/* IFF_xxx values */
+		ifi->ifi_myflags = myflags;	/* IFI_xxx values */
+#if defined(SIOCGIFMTU) && defined(HAVE_STRUCT_IFREQ_IFR_MTU)
+		Ioctl(sockfd, SIOCGIFMTU, &ifrcopy);
+		ifi->ifi_mtu = ifrcopy.ifr_mtu;
+#else
+		ifi->ifi_mtu = 0;
+#endif
+		memcpy(ifi->ifi_name, ifr->ifr_name, IFI_NAME);
+		ifi->ifi_name[IFI_NAME-1] = '\0';
+		/* If the sockaddr_dl is from a different interface, ignore it */
+		if (sdlname == NULL || strcmp(sdlname, ifr->ifr_name) != 0)
+			idx = hlen = 0;
+		ifi->ifi_index = idx;
+		ifi->ifi_hlen = hlen;
+		if (ifi->ifi_hlen > IFI_HADDR)
+			ifi->ifi_hlen = IFI_HADDR;
+		if (hlen)
+			memcpy(ifi->ifi_haddr, haddr, ifi->ifi_hlen);
+/* end get_ifi_info3 */
+/* include get_ifi_info4 */
+		switch (ifr->ifr_addr.sa_family) {
+		case AF_INET:
+			sinptr = (struct sockaddr_in *) &ifr->ifr_addr;
+			ifi->ifi_addr = calloc(1, sizeof(struct sockaddr_in));
+			memcpy(ifi->ifi_addr, sinptr, sizeof(struct sockaddr_in));
+
+#ifdef	SIOCGIFBRDADDR
+			if (flags & IFF_BROADCAST) {
+				ioctl(sockfd, SIOCGIFBRDADDR, &ifrcopy);
+				sinptr = (struct sockaddr_in *) &ifrcopy.ifr_broadaddr;
+				ifi->ifi_brdaddr = calloc(1, sizeof(struct sockaddr_in));
+				memcpy(ifi->ifi_brdaddr, sinptr, sizeof(struct sockaddr_in));
+			}
+#endif
+
+#ifdef	SIOCGIFDSTADDR
+			if (flags & IFF_POINTOPOINT) {
+				ioctl(sockfd, SIOCGIFDSTADDR, &ifrcopy);
+				sinptr = (struct sockaddr_in *) &ifrcopy.ifr_dstaddr;
+				ifi->ifi_dstaddr = calloc(1, sizeof(struct sockaddr_in));
+				memcpy(ifi->ifi_dstaddr, sinptr, sizeof(struct sockaddr_in));
+			}
+#endif
+			break;
+
+		case AF_INET6:
+			sin6ptr = (struct sockaddr_in6 *) &ifr->ifr_addr;
+			ifi->ifi_addr = calloc(1, sizeof(struct sockaddr_in6));
+			memcpy(ifi->ifi_addr, sin6ptr, sizeof(struct sockaddr_in6));
+
+#ifdef	SIOCGIFDSTADDR
+			if (flags & IFF_POINTOPOINT) {
+				ioctl(sockfd, SIOCGIFDSTADDR, &ifrcopy);
+				sin6ptr = (struct sockaddr_in6 *) &ifrcopy.ifr_dstaddr;
+				ifi->ifi_dstaddr = calloc(1, sizeof(struct sockaddr_in6));
+				memcpy(ifi->ifi_dstaddr, sin6ptr, sizeof(struct sockaddr_in6));
+			}
+#endif
+			break;
+
+		default:
+			break;
+		}
+	}
+	free(buf);
+	return(ifihead);	/* pointer to first structure in linked list */
+}
+/* end get_ifi_info4 */
+
+/* include free_ifi_info */
+void
+free_ifi_info(struct ifi_info *ifihead)
+{
+	struct ifi_info	*ifi, *ifinext;
+
+	for (ifi = ifihead; ifi != NULL; ifi = ifinext) {
+		if (ifi->ifi_addr != NULL)
+			free(ifi->ifi_addr);
+		if (ifi->ifi_brdaddr != NULL)
+			free(ifi->ifi_brdaddr);
+		if (ifi->ifi_dstaddr != NULL)
+			free(ifi->ifi_dstaddr);
+		ifinext = ifi->ifi_next;	/* can't fetch ifi_next after free() */
+		free(ifi);					/* the ifi_info{} itself */
+	}
+}
+/* end free_ifi_info */
+
+struct ifi_info *
+Get_ifi_info(int family, int doaliases)
+{
+	struct ifi_info	*ifi;
+
+	if ( (ifi = get_ifi_info(family, doaliases)) == NULL)
+		err_quit("get_ifi_info error");
+	return(ifi);
+}
+
+int
+sock_get_port(const struct sockaddr *sa, socklen_t salen)
+{
+	switch (sa->sa_family) {
+	case AF_INET: {
+		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+		return(sin->sin_port);
+	}
+
+#ifdef	IPV6
+	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6 *) sa;
+
+		return(sin6->sin6_port);
+	}
+#endif
+	}
+
+    return(-1);		/* ??? */
+}
+
+void
+sock_set_port(struct sockaddr *sa, socklen_t salen, int port)
+{
+	switch (sa->sa_family) {
+	case AF_INET: {
+		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+		sin->sin_port = port;
+		return;
+	}
+
+#ifdef	IPV6
+	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6 *) sa;
+
+		sin6->sin6_port = port;
+		return;
+	}
+#endif
+	}
+
+    return;
+}
+
+void
+sock_set_addr(struct sockaddr *sa, socklen_t salen, const void *addr)
+{
+	switch (sa->sa_family) {
+	case AF_INET: {
+		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+		memcpy(&sin->sin_addr, addr, sizeof(struct in_addr));
+		return;
+	}
+
+#ifdef	IPV6
+	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6 *) sa;
+
+		memcpy(&sin6->sin6_addr, addr, sizeof(struct in6_addr));
+		return;
+	}
+#endif
+	}
+
+    return;
+}
+
+void
+sock_set_wild(struct sockaddr *sa, socklen_t salen)
+{
+	const void	*wildptr;
+
+	switch (sa->sa_family) {
+	case AF_INET: {
+		static struct in_addr	in4addr_any;
+
+		in4addr_any.s_addr = htonl(INADDR_ANY);
+		wildptr = &in4addr_any;
+		break;
+	}
+
+#ifdef	IPV6
+	case AF_INET6: {
+		wildptr = &in6addr_any;
+		break;
+	}
+#endif
+
+	default:
+		return;
+	}
+	sock_set_addr(sa, salen, wildptr);
+    return;
+}
+
+int
+udp_client(const char *host, const char *serv, void **saptr, socklen_t *lenp)
+{
+	int				sockfd, n;
+	struct addrinfo	hints, *res, *ressave;
+
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+
+	if ( (n = getaddrinfo(host, serv, &hints, &res)) != 0)
+		err_quit("udp_client error for %s, %s: %s",
+				 host, serv, gai_strerror(n));
+	ressave = res;
+
+	do {
+		sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (sockfd >= 0)
+			break;		/* success */
+	} while ( (res = res->ai_next) != NULL);
+
+	if (res == NULL)	/* errno set from final socket() */
+		err_sys("udp_client error for %s, %s", host, serv);
+
+	*saptr = malloc(res->ai_addrlen);
+	memcpy(*saptr, res->ai_addr, res->ai_addrlen);
+	*lenp = res->ai_addrlen;
+
+	freeaddrinfo(ressave);
+
+	return(sockfd);
+}
+
+void
+mydg_echo(int sockfd, SA *myaddr, socklen_t salen)
+{
+	int			n;
+	char		mesg[MAXLINE];
+	socklen_t	len;
+	struct sockaddr *cli;
+
+	cli = malloc(salen);
+
+	for ( ; ; ) {
+		len = salen;
+		n = recvfrom(sockfd, mesg, MAXLINE, 0, cli, &len);
+		printf("child %d, datagram from %s",
+			   getpid(), sock_ntop(cli, len));
+		printf(", to %s\n", sock_ntop(myaddr, salen));
+
+		sendto(sockfd, mesg, n, 0, cli, len);
+	}
+}
+
+int
+main(int argc, char **argv)
+{
+	int sockfd, family, port;
+	const int on = 1;
+	pid_t pid;
+	socklen_t salen;
+	struct sockaddr *sa, *wild;
+	struct ifi_info *ifi, *ifihead;
+
+	if (argc == 2)
+		sockfd = udp_client(NULL, argv[1], (void **) &sa, &salen);
+	else if (argc == 3)
+		sockfd = udp_client(argv[1], argv[2], (void **) &sa, &salen);
+	else
+		err_quit("usage: udpserv04 [ <host> ] <service or port>");
+	family = sa->sa_family;
+	port = sock_get_port(sa, salen);
+	close(sockfd);		/* we just want family, port, salen */
+
+	for (ifihead = ifi = Get_ifi_info(family, 1);
+		 ifi != NULL; ifi = ifi->ifi_next) {
+
+			/*4bind unicast address */
+		sockfd = socket(family, SOCK_DGRAM, 0);
+		setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+		sock_set_port(ifi->ifi_addr, salen, port);
+		bind(sockfd, ifi->ifi_addr, salen);
+		printf("bound %s\n", Sock_ntop(ifi->ifi_addr, salen));
+
+		if ( (pid = fork()) == 0) {		/* child */
+			mydg_echo(sockfd, ifi->ifi_addr, salen);
+			exit(0);		/* never executed */
+		}
+
+		if (ifi->ifi_flags & IFF_BROADCAST) {
+				/* 4try to bind broadcast address */
+			sockfd = socket(family, SOCK_DGRAM, 0);
+			setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+			sock_set_port(ifi->ifi_brdaddr, salen, port);
+			if (bind(sockfd, ifi->ifi_brdaddr, salen) < 0) {
+				if (errno == EADDRINUSE) {
+					printf("EADDRINUSE: %s\n",
+						   sock_ntop(ifi->ifi_brdaddr, salen));
+					close(sockfd);
+					continue;
+				} else
+					err_sys("bind error for %s",
+							sock_ntop(ifi->ifi_brdaddr, salen));
+			}
+			printf("bound %s\n", sock_ntop(ifi->ifi_brdaddr, salen));
+
+			if ( (pid = fork()) == 0) {		/* child */
+				mydg_echo(sockfd, ifi->ifi_brdaddr, salen);
+				exit(0);		/* never executed */
+			}
+		}
+	}
+
+		/* 4bind wildcard address */
+	sockfd = socket(family, SOCK_DGRAM, 0);
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+	wild = malloc(salen);
+	memcpy(wild, sa, salen);	/* copy family and port */
+	sock_set_wild(wild, salen);
+
+	bind(sockfd, wild, salen);
+	printf("bound %s\n", sock_ntop(wild, salen));
+
+	if ( (pid = fork()) == 0) {		/* child */
+		mydg_echo(sockfd, wild, salen);
+		exit(0);		/* never executed */
+	}
+	exit(0);
+}
+
+
