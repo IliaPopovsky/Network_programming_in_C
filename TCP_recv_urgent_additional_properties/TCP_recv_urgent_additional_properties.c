23b40a9fd768023783c47bdb8c86b098c36c3e9e
Ilia Popovsky <popovskyilia@gmail.com>
2025-06-13 15:51:04 +0300

    first version project

---
+++ TCP_recv_urgent_additional_properties/TCP_recv_urgent_additional_properties.c
@@ -0,0 +1,154 @@
+// TCP_recv_urgent_additional_properties.c
+#include	"unp.h"
+
+#include	<stdarg.h>		/* ANSI C header file */
+#include	<syslog.h>		/* for syslog() */
+
+int		daemon_proc;		/* set nonzero by daemon_init() */
+
+static void
+err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int		errno_save, n;
+	char	buf[MAXLINE + 1];
+
+	errno_save = errno;		/* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, MAXLINE, fmt, ap);	/* safe */
+#else
+	vsprintf(buf, fmt, ap);					/* not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf + n, MAXLINE - n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		/* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+void
+err_quit(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/* Fatal error related to system call
+ * Print message and terminate */
+
+void
+err_sys(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+int
+tcp_listen(const char *host, const char *serv, socklen_t *addrlenp)
+{
+	int				listenfd, n;
+	const int		on = 1;
+	struct addrinfo	hints, *res, *ressave;
+
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ( (n = getaddrinfo(host, serv, &hints, &res)) != 0)
+		err_quit("tcp_listen error for %s, %s: %s",
+				 host, serv, gai_strerror(n));
+	ressave = res;
+
+	do {
+		listenfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (listenfd < 0)
+			continue;		/* error, try next one */
+
+		setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+		if (bind(listenfd, res->ai_addr, res->ai_addrlen) == 0)
+			break;			/* success */
+
+		close(listenfd);	/* bind error, close and try next one */
+	} while ( (res = res->ai_next) != NULL);
+
+	if (res == NULL)	/* errno from final socket() or bind() */
+		err_sys("tcp_listen error for %s, %s", host, serv);
+
+	listen(listenfd, LISTENQ);
+
+	if (addrlenp)
+		*addrlenp = res->ai_addrlen;	/* return size of protocol address */
+
+	freeaddrinfo(ressave);
+
+	return(listenfd);
+}
+/* end tcp_listen */
+
+/*
+ * We place the wrapper function here, not in wraplib.c, because some
+ * XTI programs need to include wraplib.c, and it also defines
+ * a Tcp_listen() function.
+ */
+
+int
+Tcp_listen(const char *host, const char *serv, socklen_t *addrlenp)
+{
+	return(tcp_listen(host, serv, addrlenp));
+}
+
+int		listenfd, connfd;
+
+void	sig_urg(int);
+
+int
+main(int argc, char **argv)
+{
+	int		size;
+
+	if (argc == 2)
+		listenfd = Tcp_listen(NULL, argv[1], NULL);
+	else if (argc == 3)
+		listenfd = Tcp_listen(argv[1], argv[2], NULL);
+	else
+		err_quit("usage: TCP_recv_urgent_additional_properties [ <host> ] <port#>");
+
+	size = 4096;
+	setsockopt(listenfd, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
+
+	connfd = accept(listenfd, NULL, NULL);
+
+	signal(SIGURG, sig_urg);
+	fcntl(connfd, F_SETOWN, getpid());
+
+	for ( ; ; )
+		pause();
+}
+
+void
+sig_urg(int signo)
+{
+	int		n;
+	char	buff[2048];
+
+	printf("SIGURG received\n");
+	n = recv(connfd, buff, sizeof(buff)-1, MSG_OOB);
+	buff[n] = 0;		/* null terminate */
+	printf("read %d OOB byte\n", n);
+}
