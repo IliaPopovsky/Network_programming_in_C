cd49009e9a7180926b642724b62a211c5a434f4f
Ilia Popovsky <popovskyilia@gmail.com>
2025-06-14 16:05:09 +0300

    first version project

---
+++ TCP_send_urgent_data_additional_properties/TCP_send_urgent_data_additional_properties.c
@@ -0,0 +1,133 @@
+// TCP_send_urgent_data_additional_properties.c
+#include	"unp.h"
+
+#include	<stdarg.h>		/* ANSI C header file */
+#include	<syslog.h>		/* for syslog() */
+
+int		daemon_proc;		/* set nonzero by daemon_init() */
+
+static void
+err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int		errno_save, n;
+	char	buf[MAXLINE + 1];
+
+	errno_save = errno;		/* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, MAXLINE, fmt, ap);	/* safe */
+#else
+	vsprintf(buf, fmt, ap);					/* not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf + n, MAXLINE - n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		/* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+void
+err_quit(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/* Fatal error related to system call
+ * Print message and terminate */
+
+void
+err_sys(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+int
+tcp_connect(const char *host, const char *serv)
+{
+	int				sockfd, n;
+	struct addrinfo	hints, *res, *ressave;
+
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ( (n = getaddrinfo(host, serv, &hints, &res)) != 0)
+		err_quit("tcp_connect error for %s, %s: %s",
+				 host, serv, gai_strerror(n));
+	ressave = res;
+
+	do {
+		sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (sockfd < 0)
+			continue;	/* ignore this one */
+
+		if (connect(sockfd, res->ai_addr, res->ai_addrlen) == 0)
+			break;		/* success */
+
+		close(sockfd);	/* ignore this one */
+	} while ( (res = res->ai_next) != NULL);
+
+	if (res == NULL)	/* errno set from final connect() */
+		err_sys("tcp_connect error for %s, %s", host, serv);
+
+	freeaddrinfo(ressave);
+
+	return(sockfd);
+}
+/* end tcp_connect */
+
+/*
+ * We place the wrapper function here, not in wraplib.c, because some
+ * XTI programs need to include wraplib.c, and it also defines
+ * a Tcp_connect() function.
+ */
+
+int
+Tcp_connect(const char *host, const char *serv)
+{
+	return(tcp_connect(host, serv));
+}
+
+int
+main(int argc, char **argv)
+{
+	int		sockfd, size;
+	char	buff[16384];
+
+	if (argc != 3)
+		err_quit("usage: TCP_send_urgent_data_additional_properties <host> <port#>");
+
+	sockfd = Tcp_connect(argv[1], argv[2]);
+
+	size = 32768;
+	setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &size, sizeof(size));
+
+	write(sockfd, buff, 16384);
+	printf("wrote 16384 bytes of normal data\n");
+	sleep(5);
+
+	send(sockfd, "a", 1, MSG_OOB);
+	printf("wrote 1 byte of OOB data\n");
+
+	write(sockfd, buff, 1024);
+	printf("wrote 1024 bytes of normal data\n");
+
+	exit(0);
+}
