15afadec5fd8b3f1d50a0a17db77f34e96c9188e
IliaPopovsky <popovskyilia@gmail.com>
2025-07-24 12:11:04 +0300

    first version project

---
+++ names_paral_echo_TCP_server_threads/names_paral_echo_TCP_server_threads.c
@@ -0,0 +1,230 @@
+// names_paral_echo_TCP_server_threads.c
+#include "unpthread.h"
+
+#include <stdarg.h>
+#include <syslog.h>		                                                /* for syslog() */
+
+int daemon_proc;		                                                /* set nonzero by daemon_init() */
+
+static void err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int errno_save, n;
+	char buf[MAXLINE];
+
+	errno_save = errno;		                                        /* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, sizeof(buf), fmt, ap);	                                 /* this is safe */
+#else
+	vsprintf(buf, fmt, ap);					          /* this is not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf+n, sizeof(buf)-n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		                                 /* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+void err_quit(const char *fmt, ...)
+{
+	va_list ap;
+        va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void err_sys(const char *fmt, ...)
+{
+	va_list ap;
+        va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+int Socket(int family, int type, int protocol)                                 // Наша собственная функция-обертка для функции socket.
+{
+  int n;
+  if( (n = socket(family, type, protocol)) < 0)
+       err_sys("socket error");
+  return (n);
+}
+
+void Bind(int fd, const struct sockaddr *sa, socklen_t salen)                  // Наша собственная функция-обертка для функции bind.
+{
+	if (bind(fd, sa, salen) < 0)
+		err_sys("bind error");
+}
+
+void Listen(int fd, int backlog)                                               // Наша собственная функция-обертка для функции listen.
+{
+	char	*ptr;
+
+		/*4can override 2nd argument with environment variable */
+	if ( (ptr = getenv("LISTENQ")) != NULL)
+		backlog = atoi(ptr);
+
+	if (listen(fd, backlog) < 0)
+		err_sys("listen error");
+}
+
+void Write(int fd, void *ptr, size_t nbytes)                                   // Наша собственная функция-обертка для функции write.
+{
+	if (write(fd, ptr, nbytes) != nbytes)
+		err_sys("write error");
+}
+
+void Close(int fd)                                                             // Наша собственная функция-обертка для функции close.
+{
+	if (close(fd) == -1)
+		err_sys("close error");
+}
+
+int Accept(int fd, struct sockaddr *sa, socklen_t *salenptr)                   // Наша собственная функция-обертка для функции accept. 
+{
+	int		n;
+
+again:
+	if ( (n = accept(fd, sa, salenptr)) < 0) {
+#ifdef	EPROTO
+		if (errno == EPROTO || errno == ECONNABORTED)
+#else
+		if (errno == ECONNABORTED)
+#endif
+			goto again;
+		else
+			err_sys("accept error");
+	}
+	return(n);
+}
+
+ssize_t	writen(int fd, const void *vptr, size_t n)           /* Write "n" bytes to a descriptor. */
+{
+	size_t		nleft;
+	ssize_t		nwritten;
+	const char	*ptr;
+
+	ptr = vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+			if (errno == EINTR)
+				nwritten = 0;		/* and call write() again */
+			else
+				return(-1);			/* error */
+		}
+
+		nleft -= nwritten;
+		ptr   += nwritten;
+	}
+	return(n);
+}
+
+
+void Writen(int fd, void *ptr, size_t nbytes)
+{
+	if (writen(fd, ptr, nbytes) != nbytes)
+		err_sys("writen error");
+}
+
+int
+tcp_listen(const char *host, const char *serv, socklen_t *addrlenp)
+{
+	int				listenfd, n;
+	const int		on = 1;
+	struct addrinfo	hints, *res, *ressave;
+
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ( (n = getaddrinfo(host, serv, &hints, &res)) != 0)
+		err_quit("tcp_listen error for %s, %s: %s",
+				 host, serv, gai_strerror(n));
+	ressave = res;
+
+	do {
+		listenfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (listenfd < 0)
+			continue;		/* error, try next one */
+
+		setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+		if (bind(listenfd, res->ai_addr, res->ai_addrlen) == 0)
+			break;			/* success */
+
+		Close(listenfd);	/* bind error, close and try next one */
+	} while ( (res = res->ai_next) != NULL);
+
+	if (res == NULL)	/* errno from final socket() or bind() */
+		err_sys("tcp_listen error for %s, %s", host, serv);
+
+	Listen(listenfd, LISTENQ);
+
+	if (addrlenp)
+		*addrlenp = res->ai_addrlen;	/* return size of protocol address */
+
+	freeaddrinfo(ressave);
+
+	return(listenfd);
+}
+
+
+int Tcp_listen(const char *host, const char *serv, socklen_t *addrlenp)
+{
+	return(tcp_listen(host, serv, addrlenp));
+}
+
+void str_echo(int sockfd)
+{
+   ssize_t n;
+   char buf[MAXLINE];
+   
+   for(;;){
+     if( (n = read(sockfd, buf, MAXLINE)) == 0)
+        return;            /* соединение закрыто с другого конца */
+        
+     Writen(sockfd, buf, n); 
+     printf("Ответ сервера отправлен!\n"); 
+   }
+}
+
+static void *doit(void *arg)
+{
+	pthread_detach(pthread_self());
+	str_echo((int) arg);	/* same function as before */
+	Close((int) arg);		/* done with connected socket */
+	return(NULL);
+}
+
+int main(int argc, char **argv)
+{
+	int				listenfd, connfd;
+	pthread_t		tid;
+	socklen_t		addrlen, len;
+	struct sockaddr	*cliaddr;
+
+	if (argc == 2)
+		listenfd = Tcp_listen(NULL, argv[1], &addrlen);
+	else if (argc == 3)
+		listenfd = Tcp_listen(argv[1], argv[2], &addrlen);
+	else
+		err_quit("usage: names_paral_echo_TCP_server_threads [ <host> ] <service or port>");
+
+	cliaddr = malloc(addrlen);
+
+	for ( ; ; ) {
+		len = addrlen;
+		connfd = Accept(listenfd, cliaddr, &len);
+		pthread_create(&tid, NULL, &doit, (void *) connfd);
+	}
+}
+ 
